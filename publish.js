4d4aaefa253afe6728e8b4633b07747dc140e419
danyg <dalgo86@gmail.com>
2013-04-28 20:33:44 +0200

    FIX: Tree generation in Panel view

--- publish.js
+++ publish.js
@@ -4,8 +4,6 @@ exports.publish = function(data, opts) {
 	require.paths.push(opts.template + '/node_modules');
 	var myPublisher = require('jd3t/myPublisher.js');
 
-	console.log(opts);
-
 	console.log('Init jsdoc3Template');
 
 	myPublisher.log.debugOn = true; // opts??
