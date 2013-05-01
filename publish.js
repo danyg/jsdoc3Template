e2721dc7113899332ea369fe0808cdd5ac0509b0
danyg <dalgo86@gmail.com>
2013-05-01 15:54:37 +0200

    FIX: Now Work with Windows Paths

--- publish.js
+++ publish.js
@@ -1,7 +1,20 @@
+var path = require('jsdoc/path');
+var npath = require('path');
+
+function resolveSourcePath(filepath) {
+	var r = npath.resolve(process.cwd(), filepath);
+	r = r.replace(process.cwd() + npath.sep, '');
+	r = r.replace(npath.sep, '/');
+	return r;
+}
 
 
 exports.publish = function(data, opts) {
-	require.paths.push(opts.template + '/node_modules');
+//	opts.destination = resolveSourcePath(opts.destination);
+//	opts.template = resolveSourcePath(opts.template);
+
+	require.paths.push(resolveSourcePath(opts.template) + '/node_modules');
+
 	var myPublisher = require('jd3t/myPublisher.js');
 
 	console.log('Init jsdoc3Template');
